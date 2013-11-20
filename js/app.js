356030b5115e0a4ca3e7f5c04a77c87d889a8a62
Brandon Mills <mills.brandont@gmail.com>
2013-11-19 21:16:31 -0500

    Add todos

--- js/app.js
+++ js/app.js
@@ -2,4 +2,16 @@ var todoapp = angular.module('todoapp', []);
 
 todoapp.controller('TodoCtrl', function TodoCtrl($scope) {
 
+	$scope.todos = [];
+	$scope.newTodo = '';
+
+	$scope.addTodo = function () {
+		$scope.todos.push({
+			title: $scope.newTodo,
+			completed: false
+		});
+
+		$scope.newTodo = '';
+	};
+
 });
